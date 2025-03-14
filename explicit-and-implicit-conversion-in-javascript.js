b266624643372cebaad37b6f4c04c3824843dd8d
BryJime <bry.jimenex@gmail.com>
2025-03-13 17:48:22 -0700

    Completed Assignment for Explicit and Implict Conversion

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,34 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = Number("5") - 2;    // Converted string 5 into a number to avoid potential combining of strings.
+console.log("The result is: " + result);  // This works because it fully converts "5" into an integer. 
 
-let isValid = Boolean("false");
-if (isValid) {
+let isValid = Boolean("true");   // Changed the booleen value name to "true" because its confusing when its a truthy and "false". 
+if (isValid) {                   
     console.log("This is valid!");
 }
+  else(isValid==0)           // Added an else function to complete the if statment and a message for a fasly value. 
+    isValid = Boolean(NaN);     // There needs to be a function for when the booleen variable is false.
+    console.log("This is invalid!");
+  
 
-let age = "25";
-let totalAge = age + 5;
+
+let age = "25";        
+let totalAge = Number(age) + 5;  // Converted age variable from a string to a number so that the actual age number can be added.
 console.log("Total Age: " + totalAge);
+ 
+
+
+console.log("\n"); 
+
+
+// My Own Examples
+// Example 1: Implicit
+let numToString = "This number is " + 100;
+console.log(numToString);
+
+// Example 2: Explicit
+let num = 898;
+let word = String(num);
+console.log(word);
